981c5b07401c22a82642e99eb54465c3acfd9937
Miguel Angel de la Fuente <mdelafuente@boslan.com>
2019-10-25 20:47:50 +0200

    Refactoring

--- IDRAPI/IDRAPI_M.py
+++ IDRAPI/IDRAPI_M.py
@@ -1,35 +1,32 @@
-import sys
-sys.path.insert(0, '../source/')
 
-import Utilidades
-from joblib import dump, load
-from APIs import AEMET as aemet
-from APIs import MODIS as modis
-from APIs import IGN as ign
+from joblib import load
+import os
 import datetime
 import pandas as pd
 import numpy as np
 import webbrowser
-import os
 
-#token="eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJGdWVudGViZXJtdWRlekBnbWFpbC5jb20iLCJqdGkiOiI3MjMzMDA5Yi0zYmUwLTQzMTgtYWIxZC0yMzJmM2Y4YWQxNTYiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTU2MDgwMTczMCwidXNlcklkIjoiNzIzMzAwOWItM2JlMC00MzE4LWFiMWQtMjMyZjNmOGFkMTU2Iiwicm9sZSI6IiJ9.N02GVYXuX9u-FIG5ljUktrDus4elnM-iVG7jthW-eg8"
+import Utilidades
+from APIs import AEMET
+from APIs import MODIS
+from APIs import IGN
 
 def get_meteo_data(token,id_estacion):
     fechas = get_date_actual(5)
     fecha_inicial = fechas['fecha_inicial']
     fecha_final = fechas['fecha_final']
 
-    datos_meteorologicos=aemet.get_datos_meteorologicos(token,fecha_inicial,fecha_final ,id_estacion)
+    datos_meteorologicos= AEMET.get_datos_meteorologicos(token, fecha_inicial, fecha_final, id_estacion)
     return datos_meteorologicos
 
 def get_modis_data(lat,lon):
     lat=str(lat)
     lon=str(lon)
     fechas=get_date_actual(20)
-    fecha_inicial_modis_format=modis.format_modis_date(fechas['fecha_inicial'])
-    fecha_final_modis_format = modis.format_modis_date(fechas['fecha_final'])
-    ndvi=modis.get_ndvi_serie(lat,lon,fecha_inicial_modis_format,fecha_final_modis_format)
-    eto=modis.get_evapotranspiration(lat,lon,fecha_inicial_modis_format,fecha_final_modis_format)
+    fecha_inicial_modis_format= MODIS.format_modis_date(fechas['fecha_inicial'])
+    fecha_final_modis_format = MODIS.format_modis_date(fechas['fecha_final'])
+    ndvi= MODIS.get_ndvi_serie(lat, lon, fecha_inicial_modis_format, fecha_final_modis_format)
+    eto= MODIS.get_evapotranspiration(lat, lon, fecha_inicial_modis_format, fecha_final_modis_format)
 
     return {'ndvi':ndvi,'eto':eto}
 
@@ -40,7 +37,7 @@ def get_date_actual(n_days):
     return {'fecha_final':fecha_final,'fecha_inicial':fecha_inicial}
 
 def get_ign_data(lat,lng):
-    pendiente=ign.get_slope(lat,lng)
+    pendiente= IGN.get_slope(lat, lng)
     return {'pendiente':float(pendiente)}
 
 def get_data_modelo(AemetToken,lat,lng,id_estacion_meteorologica):
@@ -64,21 +61,27 @@ def get_data_modelo(AemetToken,lat,lng,id_estacion_meteorologica):
     return(parametros)
 
 def load_conf():
-    conf=pd.read_json("..\conf\conf.json")
+    '''os.chdir("..")
+    path=os.path.realpath(os.curdir)
+    path_conf=str(path + "\conf\conf.json")
+    os.chdir(os.curdir)
+    conf=pd.read_json(path_conf)'''
+    conf = pd.read_json("C:\\Users\\Miguel\\PycharmProjects\\AEMET\\conf\\conf.json")
     return dict(conf)
 
 def load_model(path_model):
     model=load(path_model)
     return model
 
-def get_superficie_quemada(AemetToken,lat,lng,id_estacion_meteorologica,path_model="..\output\models\clf.joblib"):
-    model=load_model(path_model="..\output\models\clf.joblib")
+def get_superficie_quemada(AemetToken,lat,lng,id_estacion_meteorologica,path_modelo="..\output\models\clf.joblib"):
+    model=load_model(path_modelo)
     parametros=get_data_modelo(AemetToken,lat,lng,id_estacion_meteorologica)
     sup_quemada=model.predict(parametros)
     return {"sup_quemada":sup_quemada,"parametros":parametros}
 
 def open_url_map(lat,lng):
     webbrowser.open_new_tab("https://www.google.com/maps/@?api=1&map_action=map&center=" + lat + "," + lng+"&zoom=15&basemap=terrain")
+
 #sup=get_superficie_quemada(token,'42.954656','-2.325719','1475X')
 #print(sup)
 
--- IDRAPI/IDRAPI_V.py
+++ IDRAPI/IDRAPI_V.py
@@ -1,65 +1,72 @@
-from joblib import dump, load
+import sys
+sys.path.insert(0, 'C:\\Users\\Miguel\\PycharmProjects\\AEMET\\source\\APIs')
+
 import IDRAPI_M
 from tkinter import *
 from tkinter.ttk import *
 from tkinter import scrolledtext
 
-def clicked():
-    conf = IDRAPI_M.load_conf()
-    txt.insert(END, "Obteniendo parametros.\n","a")
+def main():
+    def clicked():
+        conf = IDRAPI_M.load_conf()
+        txt.insert(END, "Obteniendo parametros.\n","a")
 
-    AemetToken = conf['AEMET_TOKEN'][0]
-    id_estacion_meteorologica=combo.get()
-    latitud=lat.get()
-    longitud=lng.get()
+        AemetToken = conf['AEMET_TOKEN'][0]
+        path_modelo=conf['path_modelo'][0]
+        id_estacion_meteorologica=combo.get()
+        latitud=lat.get()
+        longitud=lng.get()
 
-    parametros=IDRAPI_M.get_superficie_quemada(AemetToken, latitud, longitud, id_estacion_meteorologica)
-    txt.insert(END,  parametros['sup_quemada'],"n")
-    print(parametros)
+        parametros=IDRAPI_M.get_superficie_quemada(AemetToken, latitud, longitud, id_estacion_meteorologica,path_modelo)
+        txt.insert(END,  parametros['sup_quemada'],"n")
+        print(parametros)
 
-    IDRAPI_M.open_url_map(latitud,longitud)
-
-
-AemetToken=""
-
-window = Tk()
-#window.bind( '<Enter>',onFormEvent )
-window.title("IDRAPI")
-
-window.geometry('350x200')
-
-txt = Label(window, text="Hello")
-txt.grid(column=0, row=0)
-
-lat_lbl=Label(window, text="Lat")
-lat_lbl.grid(sticky=E)
-lat = Entry(window,width=10)
-lat.insert(0, "42.954656")
-lat.grid(column=1, row=1)
-
-lng_lbl=Label(window, text="Lng")
-lng_lbl.grid(sticky=E)
-lng = Entry(window,width=10)
-lng.insert(0, "-2.325719")
-lng.grid(column=1, row=2)
-
-btn = Button(window, text="Click Me", command=clicked)
-
-btn.grid(column=1,rowspan=5)
-
-combo_lbl=Label(window, text="Estacion meteórologica.")
-combo_lbl.grid(column=2, row=1)
-combo = Combobox(window)
-
-combo['values'] = ("1387E","1475X","8177A")
+        IDRAPI_M.open_url_map(latitud,longitud)
 
 
-combo.current(1)  # set the selected item
-combo.grid(column=2, row=2)
+    AemetToken=""
 
-txt = scrolledtext.ScrolledText(window, width=40, height=5)
-txt.tag_config("n", background="yellow", foreground="red")
-txt.tag_config("a", foreground="blue")
-txt.grid(columnspan=3, row=10,padx=5)
+    window = Tk()
+    #window.bind( '<Enter>',onFormEvent )
+    window.title("IDRAPI")
 
-window.mainloop()
+    window.geometry('350x200')
+
+    txt = Label(window, text="Hello")
+    txt.grid(column=0, row=0)
+
+    lat_lbl=Label(window, text="Lat")
+    lat_lbl.grid(sticky=E)
+    lat = Entry(window,width=10)
+    lat.insert(0, "42.954656")
+    lat.grid(column=1, row=1)
+
+    lng_lbl=Label(window, text="Lng")
+    lng_lbl.grid(sticky=E)
+    lng = Entry(window,width=10)
+    lng.insert(0, "-2.325719")
+    lng.grid(column=1, row=2)
+
+    btn = Button(window, text="Click Me", command=clicked)
+
+    btn.grid(column=1,rowspan=5)
+
+    combo_lbl=Label(window, text="Estacion meteórologica.")
+    combo_lbl.grid(column=2, row=1)
+    combo = Combobox(window)
+
+    combo['values'] = ("1387E","1475X","8177A")
+
+
+    combo.current(1)  # set the selected item
+    combo.grid(column=2, row=2)
+
+    txt = scrolledtext.ScrolledText(window, width=40, height=5)
+    txt.tag_config("n", background="yellow", foreground="red")
+    txt.tag_config("a", foreground="blue")
+    txt.grid(columnspan=3, row=10,padx=5)
+
+    window.mainloop()
+
+if __name__=="__main__":
+    main()
